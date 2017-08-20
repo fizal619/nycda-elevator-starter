bee9cd1214b5d613c812c9cc7db50d833ce0cb7b
Fizal Sarif <fizal.sarif@gmail.com>
2017-08-19 23:41:01 -0400

    switched to the emitter model

--- app.js
+++ app.js
@@ -16,47 +16,59 @@ class Elevator extends EventEmitter {
     // EventEmitter
     super();
 
-    // The Elevator starts at 1
+    // The Elevator starts at 1 with no one in it
     this.currentFloor = 1;
+    this.passenger = {};
 
-    //set this to true if someone is on the elevator.
-    this.passenger = false;
   }
 }
+
+
+
+
+
+
 
 // In England, elevators are called lifts :)
 const lift = new Elevator();
 
-lift.on('up', ()=>{
-  // you're going to need to add your own logic here.
-  console.log("I'm going up, wankers!");
+lift.on('up', (passenger)=>{
+  // Defensive coding, only change the passenger if one was supplied.
+  if (passenger){
+    lift.passenger = passenger;
+  }
+
+  setTimeout(()=>{
+      // Add your own code after here.
+
+      console.log("I'm going up, wankers!");
+
+      // you probably shouldn't keep the following in your version.
+      // just here for demonstration purposes.
+      lift.emit('down');
+  }, 1000)
 
 });
 
+
+
+
+
+
 lift.on('down', ()=>{
-  // same goes here.
-  console.log("Bullocks, I'm going down...");
+
+  setTimeout(()=>{
+    // Add your own code after here.
+
+    console.log("Bullocks, I'm going down...");
+  }, 1000)
 
 })
 
+
+
+
+
 // run node app.js and take note of what happens.
 // what happens if you change it to 'down'?
-lift.emit('up');
-
-// The following is the heartbeat of the Elevator
-// You can add logic to this to dictate how it reaches
-// the end state of tenants being empty and no one is on
-// the elevator. I mean lift!
-const intervalPID = setInterval( ()=>{
-  // stuff in here will be run every second
-  let tenant = tenants.pop();
-  console.log(tenant.name);
-
-  // clearInterval is needed to stop the setInterval.
-  // https://www.w3schools.com/jsref/met_win_clearinterval.asp
-  if (tenants.length === 0 && lift.passenger == false) {
-    console.log('No passengers left.');
-    clearInterval(intervalPID);
-  }
-
-}, 1000);
+lift.emit('up', tenants.pop() );
